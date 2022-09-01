09a55200fe512840f8160eea989f313eaf748689
rlgracing <rlgracing@yahoo.com.br>
2022-09-01 15:45:45 +0100

    Adding logs

--- src/main/java/com/example/springbootkafka/rest/EventController.java
+++ src/main/java/com/example/springbootkafka/rest/EventController.java
@@ -1,16 +1,19 @@
 package com.example.springbootkafka.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/event")
 public class EventController {
 
     @GetMapping("/publish")
     public ResponseEntity<String> publishEvent() {
+        log.info("it worked!");
         return ResponseEntity.ok("Hello!");
     }
 }
